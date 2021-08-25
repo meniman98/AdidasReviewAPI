87e73f14ca6daf5049b622953e97ba9186c42f76
Huncho Jack <meniman98@gmail.com>
2021-08-25 15:01:05 +0200

    the very first

---
+++ src/main/kotlin/com/jack/huncho/adidasReviewAPI/AdidasReviewApplication.kt
@@ -0,0 +1,11 @@
+package com.jack.huncho.adidasReviewAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdidasReviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdidasReviewApplication>(*args)
+}
---
+++ src/test/kotlin/com/jack/huncho/adidasReviewAPI/AdidasReviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jack.huncho.adidasReviewAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdidasReviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
